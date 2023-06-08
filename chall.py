800e77ebed2a2c8669018c87514842ef867db033
AceJayPiamthai <acejayodom@gmail.com>
2023-06-08 15:36:32 +0700

    lil fix

--- chall.py
+++
@@ -1,9 +0,0 @@
-def wordSearch(word):
-    same = 0 
-    word = word
-    sentence = "The quick brown fox jumps over the lazy dog"
-    for word in sentence.lower :
-        same +=1
-    return same
-
-input(wordSearch)
